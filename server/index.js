f85abb1ea1c610bb4199698921175eb43c4669cf
miraenoh <yes_future@naver.com>
2020-09-25 13:42:01 +0900

    Configured for server deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
