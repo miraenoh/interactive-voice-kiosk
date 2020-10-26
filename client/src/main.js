159c4fa2c7fbee85aecad58a788a03ad90e414e2
miraenoh <yes_future@naver.com>
2020-10-27 04:02:06 +0900

    Moved store.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import {store} from './store'
+import {store} from './store/store'
 
 // Use Vuesax UI framework
 import Vuesax from 'vuesax'
